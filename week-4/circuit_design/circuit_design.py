b95b783318569a82758aa6bc338595567adc7c12
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-05-22 09:20:21 +0000

    Adding prototype of py code.

--- week-4/circuit_design/circuit_design.py
+++ week-4/circuit_design/circuit_design.py
@@ -1,13 +1,64 @@
 #!/usr/bin/env python3
 
-# python3
-n, m = map(int, input().split())
-clauses = [ list(map(int, input().split())) for i in range(m) ]
+debug = True
+
+class Node:
+    """ Simple node used to hold meta-data during graph traversals """
+    def __init__(self,number):
+        self.neighbours =[]
+        self.data = None
+        self.component = None
+        self.visited = None
+
+class TwoSatSolver:    
+    """ Solver for boolean satisfiablity problem
+
+    Works by building a constrait graph from boolean clauses and using
+    reverse topological ordering of the strongly connected components
+    to compute an assigment for the variables.
+    """
+    def __init__(self,clauses,num_variables):
+        self.clauses = clauses
+        self.num_variables = num_variables
+        self.num_clauses = len(clauses)        
+        self.nodes = list(map(lambda i:Node(i),range(0,2*num_variables)))
+
+    def node_idx(self,literal):
+        """ Compute the index into nodes list for literal """
+        v = 2*abs(literal)
+        if literal < 0:
+            return v-2
+        return v-1
+
+    
+    def build_constraing_graph(clauses,num_variables):
+        """  Use clauses to construct graph over nodes by assigning neighbours """
+        
+        for clause in clauses:
+            l1,l2 = clause
+            # for each literal add edges:
+            #      -l1 -> l2
+            #      -l2 -> l1
+            # TODO : Build the constraint graph here.
+            n1,n2 = self.node_idx(l1),self.node_idx(l2)
+            self.nodes[
+        pass
+        
+    def is_satisfiable(self):
+        """ Check the satisfiability """
+        print("Clauses:%s" % self.clauses)
+
+
+def simple_test():
+    print("Start simple_test")
+    v = TwoSatSolver([[1,-1],[1,1]],2)
+    v.is_satisfiable()
+
 
 # This solution tries all possible 2^n variable assignments.
 # It is too slow to pass the problem.
 # Implement a more efficient algorithm here.
-def isSatisfiable():
+def isSatisfiableBruteForce(clauses, num_variables):
     for mask in range(1<<n):
         result = [ (mask >> i) & 1 for i in range(n) ]
         formulaIsSatisfied = True
@@ -24,9 +75,19 @@ def isSatisfiable():
             return result
     return None
 
-result = isSatisfiable()
-if result is None:
-    print("UNSATISFIABLE")
-else:
-    print("SATISFIABLE");
-    print(" ".join(str(-i-1 if result[i] else i+1) for i in range(n)))
+
+def main():
+    num_clauses, num_variables = map(int, input().split())
+    clauses = [ list(map(int, input().split())) for i in range(num_clauses) ]
+
+    
+    result = TwoSatSolver().is_satisfiable(clauses,num_variables)
+    if not result:
+        print("UNSATISFIABLE")
+    else:
+        print("SATISFIABLE");
+        print(" ".join(str(-i-1 if result[i] else i+1) for i in range(n)))
+
+
+if __name__ == "__main__":
+    main()
