99522818b2a9f3b25e99538922a938c415a45f17
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-06-04 23:01:03 +0000

    Producing hamiltonian path.

--- week-3/cleaning_apartment/cleaning_apartment.py
+++ week-3/cleaning_apartment/cleaning_apartment.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import math
 import os
 import itertools
 
@@ -45,7 +45,7 @@ class Graph:
         self.edges = [[] for i in range(0,self.num_vertices)]
         for [a,b] in relations:
             self.edges[a-1].append(b-1)
-            self.edges[b-1].append(a-1)    
+            self.edges[b-1].append(a-1)
 
     def neighbours(self,idx):
         return self.edges[idx]
@@ -64,11 +64,11 @@ class HamiltonianPath:
     def vertex_at_least_once(self,vertex):
         """Ensure that a vertex will appear in at least one of the
         possible positions """
-        clauses = []        
+        clauses = []
         for position in range(0,self.graph.num_vertices):
             clauses.append(ClauseVariable(False,position,vertex))
         return clauses
-    
+
 
     def vertices_at_least_once(self):
         """Iterate through all vertices creating clausese such that
@@ -76,7 +76,7 @@ class HamiltonianPath:
         path"""
         clauses = []
         for vertex in range(0,self.graph.num_vertices):
-            clauses.extend(self.vertex_at_least_once(vertex))
+            clauses.append(self.vertex_at_least_once(vertex))
         return clauses
 
     def vertex_at_most_once(self,vertex):
@@ -90,7 +90,7 @@ class HamiltonianPath:
                        ClauseVariable(True,vertex,p2)]
             clauses.append(clause)
         return clauses
-            
+
     def vertices_at_most_once(self):
         clauses = []
         for v in range(0,self.graph.num_vertices):
@@ -107,14 +107,14 @@ class HamiltonianPath:
                 clause.append(ClauseVariable(False,vertex,postion))
             clauses.append(clause)
         return clauses
-   
+
     def vertex_no_simultaneos(self):
         """ Ensure that two vertices can simultaneosly occupy the same position """
-        clauses = []       
+        clauses = []
         for postion in range(0,self.graph.num_vertices):
             for (v1,v2) in itertools.combinations(range(0,self.graph.num_vertices),2):
                 clauses.append([ClauseVariable(True,v1,postion),
-                                ClauseVariable(True,v1,postion)])
+                                ClauseVariable(True,v2,postion)])
         return clauses
 
     def no_non_adjacent_vertices(self):
@@ -131,16 +131,24 @@ class HamiltonianPath:
                    clauses.append(clause)
         return clauses
 
-           
+
 class ClauseVariable:
     """ Represent a clause usable by sat solver."""
     label_encodings  = []
     encoint_positions = {}
-
+    max_position = 0
+    max_vertex = 0
     def __init__(self,compliment, vertex,postion):
         self.compliment  = compliment
         self.vertex = vertex
         self.postion = postion
+
+        if ClauseVariable.max_vertex < vertex:
+            ClauseVariable.max_vertex = vertex
+
+        if ClauseVariable.max_position < postion:
+            ClauseVariable.max_position = postion
+
 
     def __repr__(self):
         return "<%s,%d,%d>" % (self.compliment,self.vertex,self.postion)
@@ -149,76 +157,46 @@ class ClauseVariable:
         return self.__repr__()
 
     @staticmethod
-    def init_encoding_labels(clauses):
-        encode = ClauseVariable.minisat_encode
+    def encoding_factor():
+        return int(math.pow(10,math.ceil(math.log(10,ClauseVariable.max_position+1))))
 
-        # create unique list of encoded variables and give them a position
-        ClauseVariable.label_encodings = list(set([abs(encode(clause_variable)) for clause in clauses for clause_variable in clause]))
-        ClauseVariable.label_encodings.sort()
 
-        # Convert from encoding to positional value
-        ClauseVariable.encoding_positions ={}
-
-        for i in range(len(ClauseVariable.label_encodings)):
-            enc = ClauseVariable.label_encodings[i]
-            ClauseVariable.encoding_positions[enc] = i
-
-    @staticmethod
-    def minisat_encode(clause):
+    def minisat_encode(self):
         """ Creaes a mapping into a clause variable from """
-        # TODO: assumes number of postions < 10
-        retval = (10*(clause.vertex + 1))+ (clause.postion + 1)
-        if clause.compliment:
+        factor = ClauseVariable.encoding_factor()
+        retval = (factor*(self.vertex + 1))+ (self.postion + 1)
+        if self.compliment:
             retval= -1*retval
         return retval
 
-    @staticmethod
-    def minisat_encode_label(clause):
-        """ Assuming label encodings were setup for all clauses first """
-        mini_encoding = ClauseVariable.minisat_encode(clause)
-        positional = ClauseVariable.encoding_positions[abs(mini_encoding)]
-        positional += 1 # avoid zeros
-        if clause.compliment:
-            positional *= -1
-        return positional
 
     @staticmethod
     def minisat_decode(clause_str):
         """Decodes a minisat output instance back into a clause variable"""
+        factor = ClauseVariable.encoding_factor()
         int_value = int(clause_str)
         compliment = (int_value < 0)
         int_value = abs(int_value)
-        postion  = (int_value % 10) -1
-        vertex = (int_value - 10 - postion)/10
+        postion  = (int_value % factor) -1
+        vertex = math.ceil(int_value/factor)-1
         return ClauseVariable(compliment,vertex,postion)
 
-    @staticmethod
-    def minisat_decode_label(label_str):
-        label_int = int(label_str)
-        compliment = 1
-        if label_int < 0:
-            compliment = -1
-        label_value = abs(label_int)
-        label_value = label_value-1  # back to zero index
-        minisat_encoding = ClauseVariable.label_encodings[label_value]
-        return ClauseVariable.minisat_decode("%d"%(compliment*minisat_encoding))
-
-    @staticmethod
-    def print_clauses(num_variables,clauses):
-        """Output format is C V : that is number of clauses followed
-by the number of variables.  Following this we lines per clause ending
-with 0. Negations represent a variable complimented.
-        """
-        ClauseVariable.init_encoding_labels(clauses)
-        num_variables = len(ClauseVariable.label_encodings)
-        num_clauses = len(clauses)
-        print("%3d %3d" %(num_clauses,num_variables))
-        for clause in clauses:
-            encode = ClauseVariable.minisat_encode_label
-            s=""
-            for clause_variable in clause:
-                s += " %3d" % encode(clause_variable)
-            print("%s 0"%s)
+#    @staticmethod
+#    def print_clauses(num_variables,clauses):
+#        """Output format is C V : that is number of clauses followed
+#by the number of variables.  Following this we lines per clause ending
+#with 0. Negations represent a variable complimented.
+#        """
+#        ClauseVariable.init_encoding_labels(clauses)
+#        num_variables = len(ClauseVariable.label_encodings)
+#        num_clauses = len(clauses)
+#        print("%3d %3d" %(num_clauses,num_variables))
+#        for clause in clauses:
+#            encode = ClauseVariable.minisat_encode_label
+#            s=""
+#            for clause_variable in clause:
+#                s += " %3d" % encode(clause_variable)
+#            print("%s 0"%s)
 
 
 class MinisatRunner:
@@ -226,10 +204,46 @@ class MinisatRunner:
     temp_in =  "/tmp/minisat.in"
     temp_out = "/tmp/minisat.out"
 
-    def __init__(self,num_variables,clauses):
-        self.num_variables = num_variables
+    def __init__(self,clauses):
         self.num_clauses = len(clauses)
         self.clauses = clauses
+        self.label_encodings  = []
+        self.encoding_positions = {}
+        self.init_encoding_labels(clauses)
+        self.num_variables = len(self.label_encodings)
+
+
+    def init_encoding_labels(self,clauses):        
+        # create unique list of encoded variables and give them a position
+        self.label_encodings = list(set([abs(clause_variable.minisat_encode())
+                                         for clause in clauses for clause_variable in clause]))
+        self.label_encodings.sort()
+        # Convert from encoding to positional value
+        self.encoding_positions ={}
+        for i in range(len(self.label_encodings)):
+            enc = self.label_encodings[i]
+            self.encoding_positions[enc] = i
+
+            #@staticmethod
+    def minisat_encode_label(self,clause):
+        """ Assuming label encodings were setup for all clauses first """
+        mini_encoding = clause.minisat_encode()
+        positional = self.encoding_positions[abs(mini_encoding)]
+        positional += 1
+        if clause.compliment:
+            positional *= -1
+        return positional
+
+    #@staticmethod
+    def minisat_decode_label(self,label_str):
+        label_int = int(label_str)
+        compliment = 1
+        if label_int < 0:
+            compliment = -1
+        label_value = abs(label_int)
+        label_value = label_value-1  # back to zero index
+        minisat_encoding = self.label_encodings[label_value]
+        return ClauseVariable.minisat_decode("%d"%(compliment*minisat_encoding))
 
     def check_sat(self):
         "Run minisat and parse output as variables"
@@ -245,7 +259,7 @@ class MinisatRunner:
             sat = in_file.readline()
             if sat.strip() == "SAT":
                 self.satisfied = True
-                decode = ClauseVariable.minisat_decode_label
+                decode = self.minisat_decode_label
                 self.variables = [decode(v) for v in  map(int,in_file.readline().split(" ")) if v != 0]
             else:
                 self.satisfied = False
@@ -255,18 +269,20 @@ class MinisatRunner:
             in_file.close()
 
     def write_minisat(self):
-        "Something something."
-        ClauseVariable.init_encoding_labels(self.clauses)
-        num_variables = len(ClauseVariable.label_encodings) #self.num_variables
+        """Write out mi """
+        #TODO move label_encodings there
+        num_variables = len(self.label_encodings) #self.num_variables
         num_clauses = self.num_clauses
         clauses = self.clauses
         outfile = MinisatRunner.temp_in
         out = open(outfile,"w")
+        #
         try:
             out.write("p cnf %3d %3d\n" %( num_variables,num_clauses))
             for clause in clauses:
                 for clause_variable in clause:
-                    out.write(" %3d"%ClauseVariable.minisat_encode_label(clause_variable));
+                    # change minisat encode_label to be here
+                    out.write(" %3d"%self.minisat_encode_label(clause_variable));
                 out.write(" 0\n")
         finally:
             out.close()
@@ -275,7 +291,7 @@ def test():
     ap = ApartementProblem(5,4,[[1,2],[2,3],[3,5],[4,5]])
     g = Graph(ap.n,ap.m,ap.relations)
     hp = HamiltonianPath(g)
-    
+
     clauses = []
     clauses.extend(hp.vertices_at_least_once())
     clauses.extend(hp.vertices_at_most_once())
@@ -283,10 +299,27 @@ def test():
     clauses.extend(hp.vertex_no_simultaneos())
     clauses.extend(hp.no_non_adjacent_vertices())
     
-    for clause in clauses:
-        print(clause)
-
+    mr = MinisatRunner(clauses)
+    variables = mr.check_sat()
+    if len(variables) > 0:
+        true_variables = [ var for var in variables if not var.compliment]
+        def sort_by_position(v1):
+            return v1.postion        
+        true_variables.sort(key=sort_by_position)
+        for v in true_variables:
+            print(v.vertex,end=' ')
+        print("")
 
 if __name__ == "__main__":
     ap = ApartementProblem.read()
     g = Graph(ap.n,ap.m,ap.relations)
+    hp = HamiltonianPath(g)
+
+    clauses = []
+    clauses.extend(hp.vertices_at_least_once())
+    clauses.extend(hp.vertices_at_most_once())
+    clauses.extend(hp.no_empty_positions())
+    clauses.extend(hp.vertex_no_simultaneos())
+    clauses.extend(hp.no_non_adjacent_vertices())
+
+    # Print clauses in appropriate order
