9547bc355cb35b172bca6722a2ab6656f012edab
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-06-08 01:43:28 +0000

    Printing out equations

--- week-2/diet/simplex.el
+++ week-2/diet/simplex.el
@@ -1,6 +1,6 @@
+(require 'an-lib)
 (require 'cl)
 (require 'gaussian)
-
 
 ;; Simplex Algorithm
 ;;
@@ -22,26 +22,27 @@
 ;;
 ;; (x_1,x_2,....x_n) - is an extreme point if and only if the subset
 ;; of A_i corresponding to non zero entries are linearly independent ?
-;; 
+;;
 ;; For example consider the linear program in standard form:
 ;;
 ;; maximize : 3*x_1 + x_2 + 2*x_3
+;;
 ;; subject to:
-;; x_1   + x_2 + 3*x_3 <= 30
-;; 2*x_1 + 2*x_2 + 5*x_3 <= 24
-;; 4*x_1 + x_2 + 2*x_3 <= 36
+;;   x_1   +   x_2 + 3*x_3   <= 30
+;; 2*x_1   + 2*x_2 + 5*x_3   <= 24
+;; 4*x_1   +   x_2 + 2*x_3   <= 36
 ;;
 ;; x_1,x_2,x_3 >= 0
 ;;
 ;; We write the above equation into slack form by introducting
-;; slack variables x_4,x_5,x_6 such that
+;; slack variables x_4,x_5,x_6 such that:
 ;;
-;; x_4  = 30 - x_1   -  x_2 - 3*x_3
-;; x_5  = 24 - 2*x_1 - 2*x_2 - 5*x_3
-;; x_6  = 36 - 4*x_1 - x_2 - 2*x_3 
+;; x_4  = 30 -   x_1   -   x_2 - 3*x_3
+;; x_5  = 24 - 2*x_1   - 2*x_2 - 5*x_3
+;; x_6  = 36 - 4*x_1   -   x_2 - 2*x_3
 ;;
 ;; x_1,x_2,x_3 >= 0 and x_4,x_5,x_6 >=0
-;; 
+;;
 ;; Each slack variable also called basic variables, different values
 ;; of slack variable specify the `tightness` of the specified
 ;; constraints. The original variables x_1,x_2,x_3 are called
@@ -81,8 +82,8 @@
 ;; x_4 = 30 - x_1 - x_2 - 3*x_3
 ;;
 ;; becomes :
-;; :: x_4 = 30 - (9 - (x_2)/4 - (x_3)/2 - (x_6)/4)  - x_2 - 3*(x_3)
-;; :: x_4 = 21 - (3*x_2)/4 - (5*x_3)/2 - (x_6)/4
+;;  x_4 = 30 - (9 - (x_2)/4 - (x_3)/2 - (x_6)/4)  - x_2 - 3*(x_3)
+;;  x_4 = 21 - (3*x_2)/4 - (5*x_3)/2 - (x_6)/4
 ;;
 ;; Following the same lines and subsitituing in corresponding
 ;; equations the pivot is going to transform into :
@@ -152,42 +153,138 @@
 ;;  variable equation's entering variable coefficient is non-negative
 ;;  to avoid division by zero.
 ;;
-;;  
+;;  if x' is the basic solution after a pivot then:
+;;  1. x'[j] = 0 for all j \in N'
 ;;
-;;References: CLRS -pg 795
+;;     Since basic solution sets all non-basic values to 0
+;;
+;;  2. x'[e] = b[l]/a[l][e]
+;;
+;;  3. x'[i] = b[i] - a[l][e]*b'[e] for all i in B'-{e}
+;;
+;;  Setting all non-basic variables to zero will leave the recently
+;;  computed coefficient for constants.
+;;
+;;  The goal of the simplex algorithm is to receive a linear program
+;;  in standard form and return the vector x_n which maximizes the
+;;  objective function. Or report if objecitive function is unbounded
+;;  or indeterminate.
+;;
+;;  InitializeSimplex(A,b,c) : To be discussed later but it tranforms
+;;  Simplex input into a slack form where the basic solution is
+;;  feasible. As we iterate through various transformations of the
+;;  simplex the basic solution will always remain feasible.
+;;
+;;  Simplex(A, b, c):
+;;
+;;     (N,B,A,b,c,v) <- InitializeSimplex(A,b,c)
+;;
+;;     Iterate through the positive coeffients non-basic variables in
+;;     the objective function.
+;;
+;;     while j in N has c[j] > 0:
+;;
+;;       e <- pick e in N such that c[e] > 0 ;; e will serve as the entering variable
+;;
+;;       Iterate through basic variable equations, looking for a
+;;       leaving variable by locating the most constrainting,
+;;       tightest bound on entering variable.
+;;
+;;       for each i in B :
+;;           if a[i][e] > 0:
+;;              delta[i] = b[i]/a[i][e]
+;;           else: delta[i]= Infinity
+;;
+;;       leaving l <- pick l in B , such that min{delta[i] for i in B}
+;;
+;;       if delta [l] == Infinity:
+;;           return "unbounded"
+;;       else:
+;;           Pivot using (leaving,entering) pair of variables
+;;           (N,B,A,b,c,v) <- Pivot([N,B,A,b,c,v],leaving,entering)
+;;
+;;     Loop Exists when all coefficients in objective function turn
+;;     negative. The basic feasible solution is also now the optimal
+;;     solution.
+;;
+;;     for i in range(0,n):
+;;         if i in B: x[i] = b[i]
+;;         else: x[i] = b[i]
+;;
+;;
+;;     Questions:
+;;        1. What is the continuous variant of linear programs ?
+;;        2. What are the dual variants ?
+;;
+;;
+;;  Returning to problem of constructing a slack form of the Simplex
+;;  Method from the Initial input , it is important to understand that
+;;  such a conversion may not be simple
+;;
+;;  Consider the linear program:
+;;     maximize { 2*x_1 - x_2 }
+;;
+;;     under :
+;;        2*x_1 - x_2    <= 2
+;;        x_1  - 5*(x_2) <= -4
+;;
+;;        x_1,x_2 >= 0
+;;
+;;     Setting x_1 = 0 and x_2 = 0  will however violate the constraint x_1 - 5*(x_2) <= -4
+;;     Thus the basic solution is not feasible. Thus the equations need to be tranformed into
+;;     A form in which the basic solution will be feasible.
+;;
+;;     The standard technique to solve this impasse is to use the
+;;     initial linear program to construct an auxiliary linear program
+;;     such that solution of the auxiliary program will suggest a
+;;     tranformation of the original program into slack form.
+;;
+;;     Auxiliary Linear Program:
+;;
+;;     maximize  -x_0 (Can be read as minimizing x_0)
+;;
+;;     under:
+;;       a_ij - x_0 <= b_i for  i=range(1,n)
+;;
+;;       x_j >= 0 in range (0,n)
+;;
+;;
+;;     Lemma: The original linear program is feasible if and only if
+;;     the auxiliary program is feasible and the optimum/maximum value
+;;     of the objective function is 0.
+;;
+;;     Thus the details of initialize simplex are given by:
+;;
+;;     InitializeSimplex(A, b, c):
+;;
+
+;;
+;;       if b[i] >= 0:
+;;          return ({1,2,3,...n},{n+1,n+2,...,n+m}, A,b,c
+;;
+;;       1. Create L_{aux}
+;;          1.1. Objective value: -x_0
+;;          1.2. \sigma(j=1..n,a_ij -x_o <= b_i)  ;; Add -x_0 to left hand side of every equation
+;;
+;;       2. x_j >= 0  for j={0,1,..n}
+;;
+;;       let l be the index of minimum b[i]
+;;       l = min{b[i] for i in n}
+;;
+;;       3. let (N, B, A, b, c, v) <- Pivot(N, B, A, b, c, v, l, 0) # pivot with x_0 as entering variable
+;;
+;;       4.
+;;
+;;
+;;  References: CLRS -pg 795
 
 (require 'cl)
 (require 'eieio)
 
-(defvar s/debug 1)
-
-(defvar s/A
-  nil "Coefficient matrix for Ax \leq b")
-
-(defvar s/b
-  nil "Bounds on the inequalities ")
-
-(defvar s/objective
-  nil "Objective function to maximize")
-
-(defvar m 0
-  "Number of variables in Ax \leq b")
-
-(defvar n 0
-  "Number of inequalities in Ax \leq b" )
-
-(defvar s/N
-  (make-bool-vector m nil)
-  "Used to maintain a set non-basic variables")
-
-(defvar s/B
-  (make-bool-vector m nil)
-  "Used to represent a set of basic variables")
-
 
 (defclass s/lp () ; No superclasses
   (
-   (N :initarg :N
+  (NB :initarg :NB
       :initform nil
       :documentation "Used to maintain set of non-basic variables ")
   (B  :initarg :B
@@ -202,32 +299,108 @@
   (c  :initarg :c
       :initform nil
       :documentation "Coefficients of the objective function"))
-    
   "Representation in basic form of a linear program upon which we
-  will be acting ")
+  will be acting.")
+
+(defun s/lp:var-string (index-coefficient)
+  (let ((index (car index-coefficient))
+        (coefficient (cadr index-coefficient)))
+    (cond
+     ((eq index 0)
+      (if coefficient
+          (format " %d " coefficient)))
+     ((< coefficient 0 ) (format " - %d x_%d " (abs coefficient) index))
+     ((> coefficient 0 ) (format " + %d x_%d " coefficient index))
+     (t ""))))
+
+(defun s/lp:equation-string (equation-vec)
+  "Convert given printable equation vector to text"
+  (setf index-coef-pairs
+        (loop for objective  across equation-vec
+              for i = 0 then (+ i 1)
+              if objective collect (list i objective)))
+  (setf variables (mapcar 's/lp:var-string index-coef-pairs))
+  (loop for v in variables
+        concat (format " %s " v)))
+
+(defun s/lp:objective-string (objective-function)
+  (format "Max : %s"
+          (s/lp:equation-string  objective-function)))
+
+(cl-defmethod s/print((lp s/lp))
+  (let ((B (oref lp B))
+        (NB (oref lp NB))
+        (A (oref lp A))
+        (b (oref lp b))
+        (c (oref lp c)))
+
+    (message (s/lp:objective-string c))
+
+    (setf equations  (loop for i in B
+                           collect (format "x_%d = %s " i
+                                           (s/lp:equation-string (aref A (- i 1))))))
+    (loop for e in equations do
+          (message e))))
+
+
+(defun s/test-print ()
+  (setq input (s/lp :c (vector nil 3 1 2 nil nil nil)
+                    :NB (list 1 2 3)
+                    :B  (list 4 5 6)
+                    :A  (vector   [nil  nil  nil  nil  nil  nil  nil]
+                                  [nil  nil  nil  nil  nil  nil  nil]
+                                  [nil  nil  nil  nil  nil  nil  nil]
+                                  [30  -1  -1  -3  nil  nil  nil]
+                                  [24  -2  -2  -5  nil  nil  nil]
+                                  [36  -4  -1  -2  nil  nil  nil])
+                    ))
+  (s/print input))
+
 
 (cl-defmethod s/pivot((lp s/lp) entering leaving)
+  "Will pivot be changing the roles of entering and leaving
+variables"
   (let ((A (oref lp A)))
-    (list 
-     (format "A %s " A)     
+    (list
+     (format "A %s " A)
      (format "Entering %d" entering)
      (format "Leaving %d" leaving))))
-  
 
-(defun s/pivot(entering leaving basis-set non-basis-set)
-  " Pivot a slack form matrix for simplex based optimization
-  `entering` - index of element becoming basic
-  `leaving`  - index of element leaving the basic element"
-  
-  ;; 1. Scale the leaving row of the slack form matrix by the leaving
-  ;;    entry form
-  
-  (loop
-   with pivot-entry = (g/table-at s/A leaving entering)
-   with b-entering  = (/ (aref s/b leaving) pivot-entry)
-   for row from 0 below (g/table-ncols s/A) do   
-   (if s/debug
-       (message "pivot-entry:%d b-entering:%d" pivot-entry b-entering))))
+;; z = 3 x_1 + x_2 + 2*x_3
+;; x_1 = undef ;; dont use these while they are non-basic?
+;; x_2 = undef
+;; x_3 = undef
+;; x_4 = 30 -    x_1  -    x_2 - 3*x_3
+;; x_5 = 24 - 2* x_1  - 2* x_2 - 5*x_3
+;; x_6 = 36 - 4* x_1  -    x_2 - 2*x_3
+;;
+;; A = [[nil , nil, nil],
+;;      [nil , nil, nil],
+;;      [nil , nil, nil],
+;;      [-1  ,  -1,  -3],
+;;      [-2  ,  -2,  -5],
+;;      [-4  ,  -1,  -2]]
+;;
+;; b  = [nil, nil, nil, 30,  24,  36]
+;; c  = [nil,   3,   1,  2, nil, nil, nil]
+;; B  = {  1,   2,   3}
+;; NB = {  4,   5,   6}
+
+
+
+;; (defun s/pivot(entering leaving basis-set non-basis-set)
+;;   " Pivot a slack form matrix for simplex based optimization
+;;   `entering` - index of element becoming basic
+;;   `leaving`  - index of element leaving the basic element"
+;;
+;;   ;; 1. Scale the leaving row of the slack form matrix by the leaving
+;;   ;;    entry form
+;;   (loop
+;;    with pivot-entry = (g/table-at s/A leaving entering)
+;;    with b-entering  = (/ (aref s/b leaving) pivot-entry)
+;;    for row from 0 below (g/table-ncols s/A) do
+;;    (if s/debug
+;;        (message "pivot-entry:%d b-entering:%d" pivot-entry b-entering))))
 
 (defun s/read-input(input-file)
   "Read in simplex from file."
@@ -247,3 +420,23 @@
 
 (ert-deftest s/test-02()
   (should (s/read-input (concat s/test-dir "/tests/02"))))
+
+
+;; (defvar s/debug 1)
+;;
+;; (defvar s/A
+;;   nil "Coefficient matrix for Ax \leq b")
+;; (defvar s/b
+;;   nil "Bounds on the inequalities ")
+;; (defvar s/objective
+;;   nil "Objective function to maximize")
+;; (defvar m 0
+;;   "Number of variables in Ax \leq b")
+;; (defvar n 0
+;;   "Number of inequalities in Ax \leq b" )
+;; (defvar s/N
+;;   (make-bool-vector m nil)
+;;   "Used to maintain a set non-basic variables")
+;; (defvar s/B
+;;   (make-bool-vector m nil)
+;;   "Used to represent a set of basic variables")
